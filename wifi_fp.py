0ff109e079c38989caa0482400c21bed5a5a13df
Or Avitan <xwave12@gmail.com>
2020-03-31 17:56:27 +0300

    Added an error calculation via validation set

--- wifi_fp.py
+++ wifi_fp.py
@@ -14,8 +14,27 @@ def initial_data_processing(df):
     wap_column_names = df.filter(regex=("WAP\d*")).columns
     df[df[wap_column_names] == 100] = -100  # 100 indicates an AP that wasn't detected
     spatial_mean = np.mean(df[wap_column_names], axis=1)
-    df[wap_column_names] = df[wap_column_names].sub(spatial_mean, axis=0)  # spatial mean normalization
+    spatial_std = np.std(df[wap_column_names], axis=1)
+    df[wap_column_names] = df[wap_column_names].sub(spatial_mean, axis=0).divide(spatial_std, axis=0)  # spatial mean normalization
     return df
+
+
+def calculate_line_location(line, rm_per_area, qtile=0.95):
+    wap_column_names = training_data.filter(regex=("WAP\d*")).columns
+    cur_bid, cur_floor = line["BUILDINGID"], line["FLOOR"]
+    radiomap = rm_per_area[cur_bid, cur_floor]
+
+    weights = sm.similarity_calculation(line[wap_column_names], radiomap[wap_column_names])
+
+    weights_qtile = np.nanquantile(weights, qtile)
+    weighted_mean_lon = np.average(radiomap.x[weights >= weights_qtile],
+                                   weights=weights[weights >= weights_qtile])
+    weighted_mean_lat = np.average(radiomap.y[weights >= weights_qtile],
+                                   weights=weights[weights >= weights_qtile])
+    error = np.sqrt((line.LONGITUDE - weighted_mean_lon) ** 2 +
+                    (line.LATITUDE - weighted_mean_lat) ** 2)
+
+    return weighted_mean_lon, weighted_mean_lat, error
 
 
 if __name__ == "__main__":
@@ -26,33 +45,25 @@ if __name__ == "__main__":
     training_data = initial_data_processing(training_data)
     validation_data = initial_data_processing(validation_data)
 
-    rm_per_area, aps_per_area = rm.create_radio_map(training_data, [1, 1])
+    rm_per_area, aps_per_area = rm.create_radio_map(training_data, [2, 2])
 
     # Assuming we know the correct floor
     # TODO: add floor estimation
-    cur_scan_id = 100
-    cur_scan_gt = validation_data.iloc[cur_scan_id]
-    cur_scan_vals, cur_bid, cur_floor = cur_scan_gt[wap_column_names], cur_scan_gt["BUILDINGID"], cur_scan_gt["FLOOR"]
+    # cur_scan_id = 20
+    # cur_scan_gt = validation_data.iloc[cur_scan_id]
+    # cur_scan_vals, cur_bid, cur_floor = cur_scan_gt[wap_column_names], cur_scan_gt["BUILDINGID"], cur_scan_gt["FLOOR"]
+    #
+    # weights, centroid, error = calculate_line_location(cur_scan_gt, rm_per_area)
 
-    relev_training_data = rm_per_area[cur_bid, cur_floor]
-    weights = sm.similarity_calculation(cur_scan_vals, relev_training_data[wap_column_names])
+    validation_results = pd.DataFrame(np.nan, columns=('FPx', 'FPy', 'error'), index=validation_data.index)
+    for index, row in validation_data.iterrows():
+        cur_x, cur_y, cur_error = calculate_line_location(row, rm_per_area, qtile=0.95)
+        validation_results.loc[index, ['FPx', 'FPy', 'error']] = [cur_x, cur_y, cur_error]
 
-    # TODO: fix location estimation via RM
-    weighted_mean_lon = np.average(relev_training_data.x[~np.isnan(weights)],
-                                   weights=weights[~np.isnan(weights)])
-    weighted_mean_lat = np.average(relev_training_data.y[~np.isnan(weights)],
-                                   weights=weights[~np.isnan(weights)])
-    error = np.sqrt((cur_scan_gt.LONGITUDE-weighted_mean_lon)**2 +
-                    (cur_scan_gt.LATITUDE-weighted_mean_lat)**2)
-    title_str = "SCAN_ID = " + str(cur_scan_id) + "\nError = " + str(np.round(error, 2)) + " [m]"
+    error_90 = np.quantile(validation_results.error, 0.9)
+    print("90% error is: " + str(error_90) + " [m]")
 
-    plt.figure()
-    # plt.scatter(relev_training_data.x, relev_training_data.y, c=weights)
-    plt.scatter(relev_training_data.x, relev_training_data.y, c=weights)
-    plt.colorbar()
-    plt.scatter(cur_scan_gt.LONGITUDE, cur_scan_gt.LATITUDE, c='r', marker='x')
-    plt.scatter(weighted_mean_lon, weighted_mean_lat, c='m', marker='*')
-    plt.title(title_str)
-    plt.xlabel("Local X"); plt.ylabel("Local Y")
-    plt.grid(alpha=0.3)
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.hist(validation_results.error, 100, density=True, histtype='step', cumulative=True)
     plt.show()
+
