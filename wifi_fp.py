6d78a3965c5622baaa4422dd4e4de80aa0de6576
Or Avitan <xwave12@gmail.com>
2020-04-27 13:27:54 +0300

    Format updates. Still diverging

--- wifi_fp.py
+++ wifi_fp.py
@@ -7,7 +7,7 @@ import warnings
 
 # ## GLOBAL DEFAULT VARIABLES
 __MINIMAL_RSSI_VALUE = 0
-__RAYLEIGH_RSSI_VAR = 1
+__RAYLEIGH_RSSI_VAR = None
 __INPUT_NAN_VALUE = 100
 __KALMAN_STARTING_ERR = 1000
 __GRID_SIZE = [2, 2]
@@ -62,51 +62,54 @@ def perform_kalman_filter_fp(df, radiomap, plot=False):
     """
     wap_column_names = df.filter(regex="WAP\d*").columns
     xx, yy = radiomap.get_map_ranges()
-    XX, YY = np.meshgrid(xx, yy)
+    sgrid = np.dstack(np.meshgrid(xx, yy))
     rm_rssi, rm_rssi_var = radiomap.get_ap_maps_ndarray(wap_column_names)
-    empty_slices = np.isnan(rm_rssi_var).all(axis=(0, 1)) | np.isnan(rm_rssi).all(axis=(0, 1))
 
     # find the relevant APs of the device-area
+    empty_slices = np.isnan(rm_rssi_var).all(axis=(0, 1)) | np.isnan(rm_rssi).all(axis=(0, 1))
     relev_ind = ~empty_slices | (~np.isnan(df[wap_column_names])).any(axis=0)
     rm_rssi, rm_rssi_var, wap_column_names = rm_rssi[:, :, relev_ind], rm_rssi_var[:, :, relev_ind], wap_column_names[relev_ind]
     rm_rssi = np.where(~np.isnan(rm_rssi), rm_rssi, __MINIMAL_RSSI_VALUE)
 
+    # initialize the data formats
     kf_results = {"loc": [], "err": [], "real_err": []}  # store results
-    ppi = np.diag(
-        [np.mean(np.diff(xx) ** 2) / 12, np.mean(np.diff(yy) ** 2) / 12])  # location minimal error (uniform in cell)
-    #sigma = np.sqrt(np.nanmean(rm_rssi_var, axis=(0, 1)))  # mean sigma of each AP
-    #min_sval = np.nanmin(rm_rssi_var[rm_rssi_var > 0])
+    ppi = np.diag([np.mean(np.diff(xx) ** 2) / 12, np.mean(np.diff(yy) ** 2) / 12])
     loc, err = [np.nanmean(xx), np.nanmean(yy)], np.diag(
         [__KALMAN_STARTING_ERR, __KALMAN_STARTING_ERR]) ** 2  # initialize location,error with no info
 
     for ind, row in df.iterrows():
+        # STATE TRANSITION
+        # stationary model (simplest one)
+        xk_min, pk_min = loc, err
+
         # ## ESTIMATION STEP
-        # state transition of stationary model (simplest one)
+        # get current step data
         rlv_cl = ~np.isnan(row[wap_column_names]) & ~np.all(rm_rssi == 0, axis=(0, 1))
         sigma = np.where(np.isnan(rm_rssi_var[:, :, rlv_cl]), 0, rm_rssi_var[:, :, rlv_cl])
         yk, rlv_rssi = row[wap_column_names[rlv_cl]], rm_rssi[:, :, rlv_cl]
 
-        # location-cell cost
-        bstk = mvn.pdf(np.dstack([XX, YY]), loc, err)
-        bstk /= np.sum(bstk)
-        bstk_s = bstk[:, :, np.newaxis]
+        # location-cell weights
+        bstk_nnrm = mvn.pdf(sgrid, xk_min, pk_min)
+        bstk_s = bstk_nnrm[:, :, np.newaxis] / np.sum(bstk_nnrm)
 
+        # prior estimations
         y_hat = np.nanmean(bstk_s * rlv_rssi, axis=(0, 1))
-        p_hat = [(bstk * XX).sum(), (bstk * YY).sum()]
+        p_hat = np.sum(sgrid * bstk_s, axis=(0, 1))
 
         # ## UPDATE STEP
-        # calculate the covariance matrices
-        ppdf = np.dstack([XX, YY]) - p_hat
+        # calculate location and rssi diffs from prior estimation
+        ppdf = sgrid - p_hat
         pydf = rlv_rssi - y_hat
 
-        ppxk = np.einsum("ijk,ijw", ppdf * bstk_s, ppdf) + ppi  # PXXk
-        ppyk = np.einsum("ijk,ijw", ppdf * bstk_s, pydf)  # PXYk
-        pyyk = np.einsum("ijk,ijw", pydf * bstk_s, pydf) + np.diag(np.nansum(sigma * bstk_s, axis=(0,1)))  # PYYk
+        # calculate the covariance matrices
+        pxxk = np.einsum("ijk,ijw", ppdf * bstk_s, ppdf) + ppi
+        pxyk = np.einsum("ijk,ijw", ppdf * bstk_s, pydf)
+        pyyk = np.einsum("ijk,ijw", pydf * bstk_s, pydf) + np.diag(np.nansum(sigma * bstk_s, axis=(0,1)))
 
         # calculate the new location estimation and eerror
-        K = np.matmul(ppyk, np.linalg.inv(pyyk))
-        loc = loc + np.matmul(K, (yk - y_hat))
-        err = ppxk - np.matmul(K, ppyk.T)
+        K = np.matmul(pxyk, np.linalg.inv(pyyk))
+        loc = xk_min + np.matmul(K, (yk - y_hat))
+        err = pxxk - np.matmul(K, pxyk.T)
         real_err = np.array([row.LONGITUDE, row.LATITUDE]) - loc
         kf_results["loc"].append(loc)
         kf_results["err"].append(err)
